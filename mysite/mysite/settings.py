2b084f14baa985ec93ca9930ed6624c854b88dfa
me <rosllarr@tutanota.com>
2023-05-03 11:19:47 +0700

    use ipython==8.12.0 instead of ptpython

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -32,7 +32,6 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'polls.apps.PollsConfig',
-    'django_ptpython',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
