b144a2978fb18e803f22f134165eba73c4dbbd31
me <rosllarr@tutanota.com>
2023-05-03 10:26:53 +0700

    create models

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'polls.apps.PollsConfig',
+    'django_ptpython',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
