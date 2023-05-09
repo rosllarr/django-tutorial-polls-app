111fd5250eee2290293654c927a7377f49894190
me <rosllarr@tutanota.com>
2023-05-09 17:29:48 +0700

    add templates

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,5 +1,29 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
+from django.template import loader
+from django.shortcuts import render, get_object_or_404
+
+from .models import Question
 
 
 def index(request):
-    return HttpResponse('Hello polls')
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    template = loader.get_template('polls/index.html')
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return HttpResponse(template.render(context, request))
+
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+
+    return render(request, 'polls/detail.html', {'question': question})
+
+
+def result(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
